3356cceaa42fb073b45c8ecc130ff8c8d5aea86c
britinimalloy <britini.malloy@outlook.com>
2017-06-20 20:00:56 -0700

    replaced usb.jpg with the .gif, created objects constructor

--- js/app.js
+++ js/app.js
@@ -8,24 +8,34 @@ var product2 = '';
 var product3 = '';
 var productName = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 console.log(productName);
-var productParent = document.getElementById('imageName');
+//var productParent = document.getElementById('imageName');
 var productImagesParent = document.getElementById('imageSet');
+var productArray = [];
+//var currentGroup = [];
+//var previousGroup = [];
 
+// var bag = new Class {}
+// bag.clicks
+//
+// array = [b53i5u9034580958, bannana]
+//
+// array[0] is the same as the bag object
+// array[0].clicks is the same as bags.clicks
 
 // =========FUNCTIONS==============================================================
 
 function startRun() {
-  product1 = generateRandomProduct(); // do this 3 times, put them into an array
+  product1 = generateRandomProduct();
   product2 = generateRandomProduct();
   product3 = generateRandomProduct();
   console.log(product1);
-  renderName(product1);
+
+  // while (product1 ==) {
+  //   //do this each time I generate a random product
+  // }
+
   renderProductImage(product1);
-  console.log(product2);
-  renderName(product2);
   renderProductImage(product2);
-  console.log(product3);
-  renderName(product3);
   renderProductImage(product3);
 }
 
@@ -37,21 +47,23 @@ function generateRandomProduct () {
   return productName[index];
 }
 
-function renderName (productName) {
-  console.log(productName);
-  var h1 = document.createElement('h1');
-  h1.textContent = productName;
-  productParent.append(h1);
-}
-
 function renderProductImage (productName) {
+  // treeImagesParent.removeChild(treeImagesParent.lastChild);
   var img = document.createElement('img');
-  img.setAttribute('src', 'images/' + productName + '.jpg'); // create if/else to go through for the different file types
-  img.setAttribute('id', productName);
+  if (productName === 'usb') {
+    img.setAttribute('src', 'images/' + productName + '.gif');
+    img.setAttribute('id', productName);
+  } else {
+    img.setAttribute('src', 'images/' + productName + '.jpg');
+    img.setAttribute('id', productName);
+  }
   productImagesParent.append(img);
 }
 
-
+for (var i = 0; i < productName.length; i++) {
+  productArray[i] = new CreateProducts(productName[i]);
+  //console.log(productArray);
+}
 
 function CreateProducts (name) { // object constructor
   this.name = name;
