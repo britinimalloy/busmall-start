d9889fe40907cc399a1c47500d3485e9deb43b00
britinimalloy <britini.malloy@outlook.com>
2017-06-23 12:08:39 -0700

    Finito!

--- js/app.js
+++ js/app.js
@@ -1,61 +1,23 @@
 'use strict';
 
-// console.log('Hello focus group!');
-// alert('Hello focus group!');
-
 //========VARIABLES================================================================
 var product1 = '';
 var product2 = '';
 var product3 = '';
-// var product4 = '';
-// var product5 = '';
-// var product6 = '';
 var currentGroup = [];
 var previousGroup = [];
 var productName = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
-//console.log(productName);
 var productImagesParent = document.getElementById('imageSet');
-var productArray = [];
-//console.log(productArray);
+var maxClicks = 0;
+var productMap = {};
+var limit = 24;
+var list = document.createElement('ul');
 
 
 // =========FUNCTIONS==============================================================
 
 function startRun() { // generate 3 random products that are non-repeating
-  product1 = generateRandomProduct();
-  while (previousGroup.includes(product1)) {
-    product1 = generateRandomProduct();
-  }
-  // console.log(product1);
-  currentGroup.push(product1);
-  // console.log(currentGroup);
-  // console.log('======================================');
-  product2 = generateRandomProduct();
-  while (currentGroup.includes(product2) || previousGroup.includes(product1)) {
-    product2 = generateRandomProduct();
-  }
-  // console.log(product2);
-  currentGroup.push(product2);
-  // console.log(currentGroup);
-  // console.log('======================================');
-  product3 = generateRandomProduct();
-  while (currentGroup.includes(product3) || previousGroup.includes(product3)) {
-    product3 = generateRandomProduct();
-  }
-  // console.log(product3);
-  currentGroup.push(product3);
-  // console.log(currentGroup);
-  // console.log('======================================');
-
-  previousGroup = currentGroup;
-  // console.log(previousGroup);
-  // console.log('======================================');
-
-  // console.log('======================================');
-  currentGroup = [];
-  // console.log(currentGroup);
-  // console.log('======================================');
-
+  previousGroup = productGenerator(previousGroup);
   renderProductImage(product1);
   renderProductImage(product2);
   renderProductImage(product3);
@@ -63,14 +25,34 @@ function startRun() { // generate 3 random products that are non-repeating
 
 startRun();
 
+function productGenerator(previousGroup) { // generate 3 random products that are non-repeating
+  product1 = generateRandomProduct();
+  while (previousGroup.includes(product1)) {
+    product1 = generateRandomProduct();
+  }
+  currentGroup.push(product1);
+
+  product2 = generateRandomProduct();
+  while (currentGroup.includes(product2) || previousGroup.includes(product2)) {
+    product2 = generateRandomProduct();
+  }
+  currentGroup.push(product2);
+
+  product3 = generateRandomProduct();
+  while (currentGroup.includes(product3) || previousGroup.includes(product3)) {
+    product3 = generateRandomProduct();
+  }
+  currentGroup.push(product3);
+  previousGroup = currentGroup;
+  return previousGroup;
+}
+
 function generateRandomProduct () {
   var index = Math.floor(Math.random() * productName.length);
-  console.log(index);
   return productName[index];
 }
 
 function renderProductImage (productName) {
-  // treeImagesParent.removeChild(treeImagesParent.lastChild);
   var img = document.createElement('img');
   if (productName === 'usb') {
     img.setAttribute('src', 'images/' + productName + '.gif');
@@ -83,8 +65,7 @@ function renderProductImage (productName) {
 }
 
 for (var i = 0; i < productName.length; i++) {
-  productArray[i] = new CreateProducts(productName[i]);
-  //console.log(productArray);
+  productMap[productName[i]] = new CreateProducts(productName[i]);
 }
 
 function CreateProducts (name) { // object constructor
@@ -92,4 +73,44 @@ function CreateProducts (name) { // object constructor
   this.img = 'images/' + name + '.jpg';
   this.timesShown = 0;
   this.timesClicked = 0;
+}
+
+productImagesParent.addEventListener ('click', clickHandler);
+
+function clickHandler (event) {
+  if (maxClicks > limit) {
+    renderList();
+    productImagesParent.removeEventListener ('click', clickHandler);
+  }
+
+  for (var i = 0; i < currentGroup.length; i++) { // record timesShown
+    productMap[currentGroup[i]].timesShown++;
+  }
+
+  currentGroup = [];
+
+  var chosen = event.target.getAttribute('id');
+  productMap[chosen].timesClicked++; // and record timesClicked
+  productImagesParent.removeChild(productImagesParent.lastChild); // clear imageSet
+  productImagesParent.removeChild(productImagesParent.lastChild);
+  productImagesParent.removeChild(productImagesParent.lastChild);
+
+  startRun(); // call productGenerator for 3 more non-repeating, non-duplicating pics
+  maxClicks++;
+}
+
+function renderList () {
+  var parentElement = document.getElementById('productList');
+  parentElement.append(list); //set up list
+
+  for (var key in productMap) { //step through array of objects:
+    var prod = productMap[key];
+    prod.name;
+    prod.timesShown;
+    prod.timesClicked;
+    var votes = 'name: ' + prod.name + ' times shown: ' + prod.timesShown + ' times clicked: ' + prod.timesClicked;
+    var item = document.createElement('li');
+    item.textContent = votes;
+    list.appendChild(item);
+  }
 }
