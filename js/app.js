a1c51d72dc3f23ba5006bfa2b0d6ee9d4b1ae700
britinimalloy <britini.malloy@outlook.com>
2017-06-23 11:56:00 -0700

    it's DONEgit status! Up to snuff for Monday's requirements, anyway...

--- js/app.js
+++ js/app.js
@@ -18,8 +18,6 @@ var list = document.createElement('ul');
 
 function startRun() { // generate 3 random products that are non-repeating
   previousGroup = productGenerator(previousGroup);
-  console.log(previousGroup);
-
   renderProductImage(product1);
   renderProductImage(product2);
   renderProductImage(product3);
@@ -28,44 +26,29 @@ function startRun() { // generate 3 random products that are non-repeating
 startRun();
 
 function productGenerator(previousGroup) { // generate 3 random products that are non-repeating
-  // console.log(previousGroup);
   product1 = generateRandomProduct();
   while (previousGroup.includes(product1)) {
     product1 = generateRandomProduct();
   }
-  console.log(product1);
   currentGroup.push(product1);
-  // console.log(currentGroup);
-  // console.log('======================================');
+
   product2 = generateRandomProduct();
   while (currentGroup.includes(product2) || previousGroup.includes(product2)) {
     product2 = generateRandomProduct();
   }
-  console.log(product2);
   currentGroup.push(product2);
-  // console.log(currentGroup);
-  // console.log('======================================');
+
   product3 = generateRandomProduct();
   while (currentGroup.includes(product3) || previousGroup.includes(product3)) {
     product3 = generateRandomProduct();
   }
-  console.log(product3);
   currentGroup.push(product3);
-  // console.log(currentGroup);
-  // console.log('======================================');
-
   previousGroup = currentGroup;
-  // console.log(previousGroup);
-  // console.log('======================================');
-
-
-  // console.log(previousGroup);
   return previousGroup;
 }
 
 function generateRandomProduct () {
   var index = Math.floor(Math.random() * productName.length);
-  // console.log(index);
   return productName[index];
 }
 
@@ -94,54 +77,40 @@ function CreateProducts (name) { // object constructor
 
 productImagesParent.addEventListener ('click', clickHandler);
 
-function clickHandler (event) { // event handler needs to:
+function clickHandler (event) {
   if (maxClicks > limit) {
     renderList();
     productImagesParent.removeEventListener ('click', clickHandler);
   }
-  // take click
-  console.log(currentGroup.length + 'yaaaay');
+
   for (var i = 0; i < currentGroup.length; i++) { // record timesShown
     productMap[currentGroup[i]].timesShown++;
-    console.log('-------');
-    console.log(productMap[currentGroup[i]]);
   }
-  // console.log('======================================');
+
   currentGroup = [];
-  // console.log(currentGroup);
-  // console.log('======================================');
 
   var chosen = event.target.getAttribute('id');
   productMap[chosen].timesClicked++; // and record timesClicked
   productImagesParent.removeChild(productImagesParent.lastChild); // clear imageSet
   productImagesParent.removeChild(productImagesParent.lastChild);
   productImagesParent.removeChild(productImagesParent.lastChild);
-  // console.log('======================================');
-  // console.log(previousGroup);
-  // console.log('======================================');
+
   startRun(); // call productGenerator for 3 more non-repeating, non-duplicating pics
-  // console.log(previousGroup);
   maxClicks++;
-  console.log(maxClicks);
 }
 
 function renderList () {
   var parentElement = document.getElementById('productList');
-  parentElement.appendChild('ul'); //set up list
+  parentElement.append(list); //set up list
 
   for (var key in productMap) { //step through array of objects:
     var prod = productMap[key];
-    console.log(key);
-    prod.name; //to display name,
-    prod.timesShown; // times shown,
-    prod.timesClicked;  // times clicked
+    prod.name;
+    prod.timesShown;
+    prod.timesClicked;
     var votes = 'name: ' + prod.name + ' times shown: ' + prod.timesShown + ' times clicked: ' + prod.timesClicked;
-  }
-  //var list = document.createElement('ul');
-  for (var i = 0; i < productMap.length; i++) {
     var item = document.createElement('li');
-    item.appendChild(votes);
+    item.textContent = votes;
     list.appendChild(item);
   }
-  //return list;
 }
